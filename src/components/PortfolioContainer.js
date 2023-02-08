746c6d6c7e731573e45fb9ed187c9dc89d83f739
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-08 16:18:37 -0700

    Added boilerplate files/folders and added TODO comments

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+// import pages and other components below
+
+export default function PortfolioContainer () {
+    // TODO: write useState for current page here
+
+    // TODO: write logic to  render page here
+
+    // TODO: write handler that changes page state
+
+    return (
+        <div>
+            {/* Components/Pages that go inside of portfolio container go here */}
+            {/* Header */}
+            {/* Pages */}
+            {/* Footer */}
+        </div>
+    )
+
+}
