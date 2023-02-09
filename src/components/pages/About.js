0c9b42fc02a6f7e5c2f26241022e888b6adf108c
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-09 15:41:31 -0700

    Added basic content for the 'About' page

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+
+    return (
+        <div>
+            <h2>About</h2>
+            <p>I’m an ex-teacher, newbie web developer interested in designing and working on websites for educational organizations and institutions. I like working with HTML and CSS, and I’m currently in the process of learning JavaScript. I have some interest in building browser games that teachers can customize for their classrooms and enhance student engagement. If you have any questions or want to get in touch, contact me via one of the methods below.</p>
+        </div>
+    )
+}
