caf478b35f1740760386f4424e5a7f868ca6d450
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-09 16:13:05 -0700

    Added renderPage and handlePageChange functions and passed them to header[A

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-export default function Header() {
+export default function Header({ currentPage, handlePageChange }) {
 
     return (
         <header className="container fixed">
             <h1 className="bg-secondary text-quaternary text-4xl ssp font-semibold m-2 p-2 rounded-md text-center md:text-left">Grant's Portfolio</h1>
-            <Navbar />
+            
+            {/* Navbar renders here? */}
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
         </header>
     )
 }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,21 +1,29 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 // import pages and other components below
+import About from "./pages/About";
 
 export default function PortfolioContainer () {
     // TODO: write useState for current page here
     const [currentPage, setCurrentPage] = useState('About');
 
     // TODO: write logic to  render page here
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />
+        }
+    }
 
     // TODO: write handler that changes page state
+    const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <div className="container h-full bg-primary">
             {/* Components/Pages that go inside of portfolio container go here */}
             {/* Header */}
-            <Header />
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
             {/* Pages */}
+            {renderPage()}
             {/* Footer */}
         </div>
     )
