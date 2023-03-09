293319bfa304aceea6b5f23a3218cd9b081032fe
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-09 15:42:02 -0700

    Added email functiionality to the contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,54 +1,57 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
     const [message, setMessage] = useState('');
+    const form = useRef();
 
-    const handleFormSubmit = (event) => {
+    const sendEmail = (e) => {
 
-        event.preventDefault();
+        e.preventDefault();
 
-        // grabs inputs
-        const email = document.getElementsByName('email');
-        const userName = document.getElementsByName('userName');
-        const userMessage = document.getElementsByName('userMessage');
+        emailjs.sendForm('service_to29jrk', 'template_ruisbfq', form.current, '-_6WN9kDpyXT6ATIh')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
 
-        // alert the user that the form isn't functional
-        if (email && userName && userMessage) {
-            setMessage('Sorry! This form doesn\'t work yet. Please email me at ggorosave@gmail.com with any inquiries.')
-        }
-        
-    }
+        setMessage("Thanks! I'll get back to you as soon as I can.")
 
+    };
+
+   
     return (
         <div className="bg-secondary mt-32 mx-auto rounded-lg w-full md:w-4/6 lg:w-2/6">
 
             {/* Under Construction */}
             <div>
-                <p className="bg-quaternary text-primary m-4 px-2 py-1 rounded-lg text-lg">
-                    <i class="fa-solid fa-screwdriver-wrench mr-2"></i>
-                    This form is still under construction. Check back in a few days. Please email me at <a href="mailto:ggorosave@gmail.com" className="font-semibold">ggorosave@gmail.com</a> with any inquiries.
+                <p className="bg-quaternary text-primary m-4 px-2 py-1 rounded-lg text-lg text-center">
+                    
+                    <i className="fa-solid fa-envelope mr-2"></i>
+                    <a href="mailto:ggorosave@gmail.com" className="font-semibold">ggorosave@gmail.com</a>
                 </p>
             </div>
 
-            <form className="bg-tertiary m-4 px-3 py-2 rounded-lg text-primary">
+            <form ref={form} className="bg-tertiary m-4 px-3 py-2 rounded-lg text-primary" onSubmit={sendEmail}>
                 <h2 className="text-xl font-bold text-center">Contact</h2>
                 <label className="block mb-3">
                     Name:
-                    <input name="userName" type="text" className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
+                    <input name="name" type="text" className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
                 </label>
                 <label className="block mb-3">
                     Email:
-                    <input name="userEmail" type="email" className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
+                    <input name="email" type="email" className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
                 </label>
                 <label className="block mb-3">
                     Message:
-                    <textarea name="userMessage" rows={6} className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
+                    <textarea name="message" rows={6} className="block rounded-lg bg-quaternary w-full mt-1 px-2 py-1" required={true} />
                 </label>
                 <button
                     type="submit"
+                    value="Send"
                     className="bg-quaternary px-4 py-1 rounded-2xl w-full font-semibold hover:bg-primary hover:text-quaternary hover:transition-colors hover:duration-150 hover:ease-out active:bg-secondary"
-                    onClick={handleFormSubmit}
                 >
                     Submit
                 </button>
